846375af418fcaeceb084d2c1ae1c92283f1835d
JasonLiu <281300169@qq.com>
2017-08-01 10:20:05 +0800

    Initial Commit

---
+++ JLAlarmClock/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  JLAlarmClock
+//
+//  Created by JasonLiu on 2017/8/1.
+//  Copyright © 2017年 JasonLiu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
